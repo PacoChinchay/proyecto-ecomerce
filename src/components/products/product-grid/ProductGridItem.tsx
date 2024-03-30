c1ed4ad0c36e9a4cef636b5c98329d66aae9a994
PacoChinchay <pacochinchay350@gmail.com>
2024-03-29 21:12:30 -0500

    style: enhance visual effect of ProductGrid component

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,23 +1,31 @@
+'use client';
+
 import { Product } from "@/interfaces"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+
+  const [displayImage, setDisplayImage] = useState( product.images[0] )
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link
       href={ product.slug }
       >
       <Image 
-        src={`/products/${ product.images[0] }`}
+        src={`/products/${ displayImage }`}
         alt={ product.title }
-        className="w-full object-cover"
+        className="w-full object-cover rounded"
         width={500}
         height={500}
+        onMouseEnter={ () => setDisplayImage(product.images[1]) }
+        onMouseLeave={ () => setDisplayImage(product.images[0]) }
       />
       </Link>
       <div className="p-4 flex flex-col">
