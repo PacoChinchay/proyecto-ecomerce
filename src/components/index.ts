040856172e1904940642d8da45f005917b8bf21a
PacoChinchay <pacochinchay350@gmail.com>
2024-03-29 20:38:48 -0500

    feat: add ProductGridItem component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 
 export * from './products/product-grid/ProductGrid'
+export * from './products/product-grid/ProductGridItem'
 
 export * from './ui/top-menu/TopMenu'
 export * from './ui/not-found/PageNotFound'
--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/interfaces";
+import { ProductGridItem } from "./ProductGridItem";
 
 interface Props {
   products: Product[];
@@ -9,7 +10,10 @@ export const ProductGrid = ({ products }: Props ) => {
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
       {
         products.map( product => (
-          <span key={ product.slug }>{ product.title }</span>
+          <ProductGridItem 
+           product={ product }
+           key={ product.slug }
+          />
         ))
       }
     </div>
