21357c7b45f8bf1039592c1e4d827c18cb4cc0d1
PacoChinchay <pacochinchay350@gmail.com>
2024-04-09 21:27:31 -0500

    feat: add ProductMobileSlideShow component

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideShow, ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -21,9 +21,18 @@ export default function EmptyPage({ params }: Props) {
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       <div className="col-span-1 md:col-span-2">
+
+        <ProductMobileSlideShow 
+          title={product.title}
+          images={product.images}
+          className="block md:hidden"
+        />
+
+
         <ProductSlideShow
           title={product.title}
           images={product.images} 
+          className="hidden md:block"
           />
       </div>
 
